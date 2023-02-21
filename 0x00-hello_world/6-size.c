8aa1a9c160cbd49ddec5ffc551f0d1c34115fda4
Sayon CAMARA <sayon.camara.505@gmail.com>
2023-02-21 03:39:37 -0800

    coorect

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,13 +6,12 @@
  */
 int main(void)
 {
- 	int a;
+	int a;
 	long int b;
 	long long int c;
 	char d;
 	float f;
-
- 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
