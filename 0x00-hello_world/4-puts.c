8ae8503e2fd3379b06b7b6c1de3d79190cafc7bf
Sayon CAMARA <sayon.camara.505@gmail.com>
2023-02-21 01:57:53 -0800

    ajout comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main
+ * main - puts Programming is like building a multilingual puzzle
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
- 	return (0);
+	return (0);
 }
