5b692acbf9e527d7d8052ea29597ca9d96c351bc
Sayon CAMARA <sayon.camara.505@gmail.com>
2023-02-21 04:58:07 -0800

    avance tast

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - print and that piece of art is useful
+ * - Due Korpar, 2015-10-19
+ * Return: Always 0 (failure)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-15\n", 59);
+       return (1);	
+}
